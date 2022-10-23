a946a7971836b4b08ef1e9fb5fac147a87f4faf7
lerte smith <lerte@qq.com>
2022-10-23 13:50:10 +0800

    更新成功案例頁面

--- pages/case.vue
+++ pages/case.vue
@@ -1,5 +1,9 @@
 <template>
-  <article></article>
+  <ul class="grid grid-cols-3 gap-4">
+    <li v-for="(item, index) in list" :key="index">
+      <img :src="item" alt="成功案例" />
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -8,6 +12,15 @@ export default {
     return {
       title: '成功案例'
     }
-  }
+  },
+  data: () => ({
+    list: [
+      require('@/assets/images/201634182219.jpg'),
+      require('@/assets/images/201634182207.jpg'),
+      require('@/assets/images/20131023184320.jpg'),
+      require('@/assets/images/20131023184446.jpg'),
+      require('@/assets/images/20131023184459.jpg')
+    ]
+  })
 }
 </script>
