40e27ed3477b3dad6ae3d520199baf68eaf79de9
lerte smith <lerte@qq.com>
2022-10-28 14:11:09 +0800

    更新代理品牌页面

--- pages/brand/_id.vue
+++ pages/brand/_id.vue
@@ -1,7 +1,38 @@
 <template>
-  <article></article>
+  <List :list="list" v-if="list.length" />
+  <section v-else>
+    <img
+      :alt="list.title"
+      v-lazy="list.image"
+      class="mx-auto rounded-md shadow-md"
+    />
+    <p>{{ list.title }}</p>
+    <p v-html="list.content"></p>
+  </section>
 </template>
 
 <script>
-export default {}
+import brands from '@/contents/brands.json'
+export default {
+  head() {
+    return {
+      title: '代理品牌'
+    }
+  },
+  async asyncData({ params, $http }) {
+    const found = brands.find((brand) => brand.id == params.id)
+    if (found) {
+      const list = await $http.$get(`/brands/${params.id}.json`)
+      return {
+        list
+      }
+    } else {
+      return {
+        list: {
+          content: '内容未找到'
+        }
+      }
+    }
+  }
+}
 </script>
